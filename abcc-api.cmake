a97ffba2bf839ba5217a0d2b629e6ab4ce274391
Andreas Johansson <ajoh@hms.se>
2025-03-06 20:00:57 +0100

    Make into CMake library

---
+++ abcc-api.cmake
@@ -0,0 +1,62 @@
+# The CMake command include_guard is compatible with CMake version 3.10 and greater.
+if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.10.0")
+# Preventing this file to be included more than once.
+   include_guard(GLOBAL)
+endif()
+
+# Complete list of source (.c) files inside the Anybus CompactCom API. 
+set(abcc_api_SRCS
+   ${ABCC_API_DIR}/src/abcc_api_select_firmware.c
+   ${ABCC_API_DIR}/src/abcc_api_handler.c
+   ${ABCC_API_DIR}/src/abcc_api_command_handler.c
+   ${ABCC_API_DIR}/src/host_objects/application_data_object.c
+   ${ABCC_API_DIR}/src/anybus_objects/anybus_file_system_interface_object.c
+)
+
+# Complete list of source (.c) files inside the Anybus CompactCom API. 
+set(abcc_api_INCS
+   ${ABCC_API_DIR}/inc/abcc_api.h
+   ${ABCC_API_DIR}/inc/abcc_api_network_settings.h
+   ${ABCC_API_DIR}/inc/abcc_api_select_firmware.h
+   ${ABCC_API_DIR}/inc/host_objects/abcc_api_command_handler_lookup.h
+   ${ABCC_API_DIR}/inc/anybus_objects/anybus_file_system_interface_object.h
+   ${ABCC_API_DIR}/src/abcc_api_object_config.h
+   ${ABCC_API_DIR}/src/abcc_api_identification.h
+   ${ABCC_API_DIR}/src/abcc_api_config.h
+   ${ABCC_API_DIR}/src/abcc_api_command_handler.h
+   ${ABCC_API_DIR}/src/host_objects/application_data_object.h
+)
+
+# Creating a library target containing the Anybus CompactCom API.
+add_library(abcc_api EXCLUDE_FROM_ALL ${abcc_api_SRCS} ${abcc_api_INCS})
+
+# Keeping the file and directory tree structure of the Anybus CompactCom API when 
+# generating IDE projects.
+source_group(TREE ${ABCC_API_DIR} PREFIX "abcc-api" FILES ${abcc_api_SRCS} ${abcc_api_INCS})
+
+# Essentially a renaming.
+set(ABCC_DRIVER_INCLUDE_DIRS ABCC_API_INCLUDE_DIRS)
+
+# The directory containing the Anybus CompactCom Driver repository.
+set(ABCC_DRIVER_DIR ${ABCC_API_DIR}/abcc-driver)
+
+# Including the Anybus CompactCom Driver's CMake module file.
+include(${ABCC_DRIVER_DIR}/abcc-driver.cmake)
+
+# Directories inside the Anybus CompactCom API containing include (.h) files to be 
+# externally accessible is appended to the list ABCC_API_INCLUDE_DIRS. Not using
+# append() command since the 'user unique' include (.h) files previously added is
+# included in ABCC_DRIVER_INCLUDE_DIRS from the previous "renaming" operation.
+set( ABCC_API_INCLUDE_DIRS
+   ABCC_DRIVER_INCLUDE_DIRS
+   ${ABCC_API_DIR}/inc
+   ${ABCC_API_DIR}/inc/host_objects
+   ${ABCC_API_DIR}/inc/anybus_objects
+)
+
+# Adding all the Anybus CompactCom API related include (.h) files to the user host 
+# Anybus CompactCom API library target.
+target_include_directories(abcc_api PRIVATE ${ABCC_API_INCLUDE_DIRS})
+
+# Link the Anybus CompactCom Driver library to the Anybus CompactCom API library.
+target_link_libraries(abcc_api PRIVATE abcc_driver)

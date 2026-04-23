7ade228972f5bcc9a60c4bf9b917cd109ddb0bb9
Dominik Wulff <dow@hms.se>
2026-04-23 18:37:58 +0200

    Update abcc_api_handler.c
    rename ABCC_NewMsgFragSize() to ABCC_SetMsgFragSize()

--- src/abcc_api_handler.c
+++ src/abcc_api_handler.c
@@ -1029,7 +1029,7 @@ void ABCC_API_Restart( void )
 #if ABCC_CFG_SPI_DYNAMIC_MSG_FRAG_LEN
 ABCC_ErrorCodeType ABCC_API_SetMsgFragSize( const UINT16 iReqMsgFragSize )
 {
-   return( ABCC_NewMsgFragSize( iReqMsgFragSize ) );
+   return( ABCC_SetMsgFragSize( iReqMsgFragSize ) );
 }
 #endif // ABCC_CFG_SPI_DYNAMIC_MSG_FRAG_LEN
 
